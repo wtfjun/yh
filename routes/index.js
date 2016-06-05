7cc86bb7c9c4017ab54a0254b50d3463bf17df80
wtfjun <13266836563@163.com>
2016-06-05 22:40:53 +0800

    2 commit

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,33 @@ router.get('/', function(req, res, next) {
 
 //新闻页面
 router.get('/news', function(req, res, next) {
-  res.render('client/news', { 
-  	title: 'Express',
-    _layoutFile: 'layout.html'
-  });
+  var page = (req.query.p)?req.query.p:1;
+  var perPage = (req.query.pr)?req.query.pr:3;
+  var count;
+  var type = req.query.type;
+  News.find({'type': type}).sort({'_id':-1}).exec(function(err, docs) {
+    count = docs.length;
+      if(page <= 0 || page >= (Math.ceil(count/3)+1)) {
+      res.redirect('/news?type='+type+'&p=1');
+      return;
+    }
+    var news = News.find({'type': type}).sort({'_id':-1}).skip((page-1)*perPage).limit(perPage).exec(function(err,news){
+      if(err) {
+        return handleError(err);
+      }
+      
+      res.render('client/news', {
+        _layoutFile: 'layout',
+        news: news,
+        type: type,
+        title: '公司新闻',
+        count: count,
+        current_page: page,
+        lately_news: docs
+
+      });
+    });
+  }); 
 });
 
 //公司简介页面
@@ -30,6 +53,26 @@ router.get('/about', function(req, res, next) {
   res.render('client/about', { 
   	title: 'Express',
     _layoutFile: 'layout.html'
+  });
+});
+
+//新闻详情页面
+router.get('/newsPage', function(req, res ,next) {
+  var id = req.query.id;
+  var type = req.query.type;
+  News.find({'type': type}).sort({'_id':-1}).exec(function(err, docs) {  
+    News.findById(id, function(err, news) {
+      if(err) {
+        return handleError(err);
+      }
+      console.log(docs);
+      res.render('client/newsPage', {
+        title: 'Express',
+        _layoutFile: 'layout.html',
+        news: news,
+        lately_news: docs
+      });
+    });
   });
 });
 
